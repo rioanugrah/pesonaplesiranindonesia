61f211597c874d2e9295cbcb898e4f2387fa3b8c
Rio Anugrah Adam Saputra <rioanugrah@Rios-MacBook-Air.local>
2025-09-07 22:24:47 +0700

    PPI 2025

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="google-site-verification" content="-agNXAZvJ7uHctHQlEr7t7q9VoOHxdpZJIDOv9womR4" />
     <link rel="icon" type="image/x-icon" href="{{ asset('frontend/assets/img/logo/favicon.png') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
